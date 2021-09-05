7eefa3644d3570f9095a88f3a2462ce15dea4279
huythanh0x <huythanh0x@gmail.com>
2021-09-05 11:53:58 +0700

    maybe it will work, iam not sure. I add port and message to automaticlly enroll course

--- background.js
+++ background.js
@@ -1,22 +1,46 @@
-chrome.runtime.onInstalled.addListener(function(){
+chrome.runtime.onInstalled.addListener(function () {
     var newURL = "https://batdaulaptrinh.com/welcome-to-udemy-extensions/";
     chrome.tabs.create({ url: newURL });
 });
 
-chrome.commands.onCommand.addListener((command) => {
-    console.log(command);
-    
+//background.js
+chrome.runtime.onConnect.addListener(function (port) {
+
+    chrome.storage.sync.get(['courses'], function(courses) {
+        if (courses.length > 0){
+            open_enroll_course(courses[0])
+            port.postMessage({ message: courses[0] });
+        }
+    });
+
+    port.onMessage.addListener(function (message) {
+
+
+
+    });
 })
 
-chrome.tabs.onCreated.addListener(function(){
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) =>{
-        chrome.tabs.sendMessage(tabs[0].id, {text: 'open a new tab'});
-    })
-})
+
+function getCourse(json) {
+    return json["results"]
+}
+function getPages(json) {
+    return json['pages']
+}
+
+function getURLEnroll(course) {
+    let idCourse = course['ImageUrl'].split('/')[5].split('_')[0]
+    let codeCoupon = course['CouponCode']
+    return "https://www.udemy.com/cart/checkout/express/course/" + idCourse + "/?discountCode=" + codeCoupon
+}
 
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse){
-        console.log(request);
-    }
-)
+function open_enroll_course(course){
+
+    console.log(course)
+    let urlEnroll = getURLEnroll(course)
+
+    chrome.tabs.update({
+        url: urlEnroll
+    });
+}
---
+++ content.js
@@ -0,0 +1,65 @@
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse){
+        if (request == 'auto'){
+            fetchAPI()
+        }
+    }
+)
+
+
+var port = chrome.runtime.connect({name: "auto_course"});
+
+
+
+port.onMessage.addListener(function(msg) {
+
+    console.log(msg.message)
+    auto_enroll()
+    remove_succesfully_enroll_course()
+
+
+});
+
+function remove_succesfully_enroll_course(){
+    chrome.storage.sync.get(['courses'], function(courses){
+        courses.shift()
+        chrome.storage.sync.set({ "courses": courses }, function () {
+            console.log('Value is set to ' + courses);
+        });
+    })
+}
+
+
+function fetchAPI(){
+
+    let apiUrl = 'https://teachinguide.azure-api.net/course-coupon?sortCol=featured&sortDir=DESC&length=10&page=2&inkw=&discount=100&language=&'
+
+    fetch(apiUrl
+    ).then((response) => {
+        return response.json();
+    })
+        .then((json) => {
+            let courses = getCourse(json)
+            // enroll_courses(courses)
+            chrome.storage.sync.set({ "courses": courses }, function () {
+                console.log('Value is set to ' + courses);
+            });
+
+        }).catch(err => { console.log(err) });
+}
+
+
+
+
+function enroll_course() {
+
+    var inputs = document.getElementsByTagName('button');
+
+    for (var i = 0; i < inputs.length; i++) {
+        console.log(inputs[i].textContent)
+        if (inputs[i]["type"] == 'submit' && inputs[i].textContent == 'Enroll now') {
+            inputs[i].click()
+        }
+    }
+
+}
