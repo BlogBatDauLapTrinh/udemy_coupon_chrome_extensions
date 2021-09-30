8c2279ec96c23217f77a1b289b5bb48012611c4a
thanh <huythanh0x@gmail.com>
2021-09-30 16:07:33 +0700

    fix stop pause and play logic

--- popup.js
+++ popup.js
@@ -5,45 +5,66 @@ let numberAvailable = document.getElementById('numberOfAvailableCoupons')
 
 chrome.storage.sync.get(['KEY_ON_OFF'], function (result) {
     let isOnSwitch = result['KEY_ON_OFF']
-    if(isOnSwitch){
+    if (isOnSwitch) {
         switchOnOff.src = '/images/green.png'
         autoState.innerText = 'AUTO IS ON'
-    }else{
+    } else {
         switchOnOff.src = '/images/red.png'
         autoState.innerText = 'AUTO IS OFF'
-    }  
+    }
 })
 switchOnOff.onclick = function (button) {
     chrome.storage.sync.get(['KEY_ON_OFF'], function (result) {
         let isOnSwitch = result['KEY_ON_OFF']
-        if(isOnSwitch){
+        if (isOnSwitch) {
             setOffSwitch()
             autoState.innerText = 'AUTO IS PAUSED'
             switchOnOff.src = '/images/red.png'
-        }else{
-            setOnSwitch()
-            autoState.innerText = 'AUTO IS ON'
-            switchOnOff.src = '/images/green.png'
-            chrome.runtime.sendMessage({message:'continue', page:2});
-        }  
+        } else {
+            autoHasBeenStoped(function (isStop) {
+                if (isStop) {
+                    alert('Auto has been stop')
+                    return
+                } else {
+                    setOnSwitch()
+                    autoState.innerText = 'AUTO IS ON'
+                    switchOnOff.src = '/images/green.png'
+                    chrome.runtime.sendMessage({ message: 'continue' });
+                }
+            })
+        }
     })
 }
 
+
 showNumberOfAvailableCouponToday()
 
-function showNumberOfAvailableCouponToday(){
+function autoHasBeenStoped(callback) {
+    chrome.storage.sync.get(['KEY_STORAGE'], function (json) {
+        let arrayCourses = json['KEY_STORAGE']
+        getCurrentIndex(function (index) {
+            if (index > arrayCourses.length) {
+                callback(true)
+            } else {
+                callback(false)
+            }
+        })
+    });
+}
+
+function showNumberOfAvailableCouponToday() {
     chrome.storage.sync.get(['KEY_AVAILABLE_COUPON'], function (result) {
         let numberOfAvailableCoupons = result['KEY_AVAILABLE_COUPON']
-        if(!numberOfAvailableCoupons){
+        if (!numberOfAvailableCoupons) {
             fetchNumberOfAvailableCouponToday()
             showNumberOfAvailableCouponToday()
-        }else{
+        } else {
             numberAvailable.innerText = 'THERE ARE ' + numberOfAvailableCoupons + ' TODAY '
         }
     })
 }
 
-function fetchNumberOfAvailableCouponToday(){
+function fetchNumberOfAvailableCouponToday() {
 
     let apiUrl = 'https://teachinguide.azure-api.net/course-coupon?sortCol=featured&sortDir=DESC&length=1&page=1&inkw=&discount=100&language='
 
@@ -51,23 +72,29 @@ function fetchNumberOfAvailableCouponToday(){
     ).then((response) => {
         return response.json();
     })
-    .then((json) => {
-        let numberOfAvailableCourse = json['recordsFiltered']
-        chrome.storage.sync.set({ 'KEY_AVAILABLE_COUPON': numberOfAvailableCourse }, function () {
-            alert('set number available ok')
-        });
-    }).catch(err => { console.log('encounter error ' + err) });
+        .then((json) => {
+            let numberOfAvailableCourse = json['recordsFiltered']
+            chrome.storage.sync.set({ 'KEY_AVAILABLE_COUPON': numberOfAvailableCourse }, function () {
+                alert('set number available ok')
+            });
+        }).catch(err => { console.log('encounter error ' + err) });
 
 }
 
-function setOffSwitch(){
-    chrome.storage.sync.set({ 'KEY_ON_OFF': false }, function () {});
+function setOffSwitch() {
+    chrome.storage.sync.set({ 'KEY_ON_OFF': false }, function () { });
 }
 
-function setOnSwitch(){
-    chrome.storage.sync.set({ 'KEY_ON_OFF': true }, function () {});
+function setOnSwitch() {
+    chrome.storage.sync.set({ 'KEY_ON_OFF': true }, function () { });
 }
 
+function getCurrentIndex(callback) {
+    chrome.storage.sync.get(['KEY_INDEX'], function (json) {
+        let currentIndex = json['KEY_INDEX']
+        callback(currentIndex)
+    })
+}
 
 let show_coupons = document.getElementById('show_coupons');
 show_coupons.onclick = function (button) {
@@ -78,12 +105,12 @@ show_coupons.onclick = function (button) {
 
 let auto_enroll1 = document.getElementById('auto_enroll1')
 auto_enroll1.onclick = function (button) {
-    chrome.runtime.sendMessage({message:'auto_click', page:1});
+    chrome.runtime.sendMessage({ message: 'auto_click', page: 1 });
 }
 
 let auto_enroll2 = document.getElementById('auto_enroll2')
 auto_enroll2.onclick = function (button) {
-    chrome.runtime.sendMessage({message:'auto_click', page:2});
+    chrome.runtime.sendMessage({ message: 'auto_click', page: 2 });
 }
 
 let options = document.getElementById('options')
